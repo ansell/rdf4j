4c42ea88dff0f185b425490b7827c1e44e97f52e
Peter Ansell <p_ansell@yahoo.com>
2016-08-17 10:42:21 +1000

    issue#293 : Also use same pattern in BackgroundGraphResult
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- core/http/client/src/main/java/org/eclipse/rdf4j/http/client/BackgroundGraphResult.java
+++ core/http/client/src/main/java/org/eclipse/rdf4j/http/client/BackgroundGraphResult.java
@@ -87,19 +87,25 @@ public class BackgroundGraphResult extends IterationWrapper<Statement, QueryEval
 	protected void handleClose()
 		throws QueryEvaluationException
 	{
-		try {
-			super.handleClose();
-		}
-		finally {
-			closed = true;
-			try {
-				in.close();
-			}
-			catch (IOException e) {
-				throw new QueryEvaluationException(e);
-			}
-			finally {
-				queue.close();
+		if (!closed) {
+			synchronized (this) {
+				if (!closed) {
+					closed = true;
+					try {
+						try {
+							super.handleClose();
+						}
+						finally {
+							in.close();
+						}
+					}
+					catch (IOException e) {
+						throw new QueryEvaluationException(e);
+					}
+					finally {
+						queue.close();
+					}
+				}
 			}
 		}
 	}
